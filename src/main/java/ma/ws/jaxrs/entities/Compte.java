abf21eab858f498f9fc98270055ee7ec19fe849c
Idriss-Elbikri <elbikriidriss@gmail.com>
2025-11-04 11:12:17 +0100

    TP7 commit

---
+++ src/main/java/ma/ws/jaxrs/entities/Compte.java
@@ -0,0 +1,29 @@
+package ma.ws.jaxrs.entities;
+
+import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@XmlRootElement
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+    @PrePersist
+    void prePersist() {
+        if (dateCreation == null) dateCreation = new Date();
+    }
+}
